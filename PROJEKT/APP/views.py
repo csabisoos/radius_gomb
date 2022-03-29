e9ad6d6d983437c8b3aa47dd9a7f9a51728c593f
Csaba Soós <csabi.soos.05@gmail.com>
2022-03-29 10:58:10 +0200

    hello world

--- PROJEKT/APP/views.py
+++ PROJEKT/APP/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from random import randint
 
-# Create your views here.
+def index(request):
+    template = 'index.html'
+    context = {}
+    return render(request, template, context)
