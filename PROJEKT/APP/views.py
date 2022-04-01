1ea19227a017518a9e5e1dbe36f2d80356aa2853
Csaba Soós <csabi.soos.05@gmail.com>
2022-04-01 11:50:43 +0200

    befejezes

--- PROJEKT/APP/views.py
+++ PROJEKT/APP/views.py
@@ -1,3 +1,4 @@
+import re
 from winsound import PlaySound
 from django.shortcuts import render
 import random
@@ -5,17 +6,28 @@ import random
 def index(request):
     pla = random.randint(0, 30)
     plb = random.randint(0, 30)
+    fela = random.randint(0, 30)
+    felb = random.randint(0, 30)
     plmo = titkos(pla, plb)
     template = 'index.html'
     context = {
         'pla':pla, 
         'plb':plb, 
         'plmo':plmo,
-        'fela':random.randint(0, 30),
-        'felb':random.randint(0, 30),
+        'fela':fela,
+        'felb':felb,
     }
     if request.method == "POST":
-        titkos(int(request.POST['regifela']), int(request.POST['regifelb'])==int(request.POST['felmo']))
+        regifela = int(request.POST['regifela'])
+        regifelb = int(request.POST['regifelb'])
+        regifelmo_user_szerint = int(request.POST['felmo'])
+        regifelmo_valojaban = titkos(regifela, regifelb)
+        #print(regifela)
+        #print(regifelb)
+        #print(regifelmo_user_szerint)
+        #print(regifelmo_valojaban)
+        context['volt_valasz'] = True
+        context['helyes'] = regifelmo_user_szerint == regifelmo_valojaban
     return render(request, template, context)
 
 def titkos(a, b):
